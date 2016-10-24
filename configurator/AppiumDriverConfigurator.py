aee345cb5b2ecde7b5f6dcd3af297d6029e25655
risan <wei.li@fengjr.com>
2016-10-24 19:23:23 +0800

    one click op

--- configurator/AppiumDriverConfigurator.py
+++ configurator/AppiumDriverConfigurator.py
@@ -1,18 +1,33 @@
 # coding=utf-8
+import os
 
-from mhlib import PATH
 from appium import webdriver
+
+PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 
 # config driver by appium environment factors
 def driver_configurator():
     desired_caps = {}
     desired_caps['platformName'] = 'Android'
-    desired_caps['platformVersion'] = '4.2'
-    desired_caps['deviceName'] = 'Android Emulator'
+    desired_caps['platformVersion'] = '6.0.1'
+    desired_caps['deviceName'] = 'MI NOTE LTE'
     desired_caps['app'] = PATH('/Users/risan/Downloads/mobile-dev-v2.3.3-debug.apk')
     print desired_caps
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    print 'driver start done '
     # return driver
+    return driver
 
 
-driver_configurator()
+driver = driver_configurator()
+
+# 点击投资理财
+
+Money_button = driver.find_element_by_id('com.fengjr.mobile:id/tvMoney')
+driver.find_element_by_android_uiautomator("android.widget.TextView")
+
+print 'button done '
+
+Money_button.click()
+
+print 'click done '
