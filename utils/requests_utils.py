5fba26fc8b20f7fd723ddf01ee376598fb7fbac6
vvvcode <857783235@qq.com>
2017-04-12 10:51:58 +0800

    Update requests_utils.py

--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -12,13 +12,3 @@ def get_request(host, uri, params, **kwargs):
     print_response_information(get_response)
     return get_response
 
-
-# get: request with params can ues dictionary
-get_response = get_request(host='https://appi.fengjr.com', uri='/app/api/v1/home/android', params={'v': '2.3'})
-if get_response.status_code == 200:
-    print 'pass'
-else:
-    print 'failed  ####  http code:', get_response.status_code
-
-# response_post = requests.post('https://passport.fengjr.com/api/v3/userAddress/updateUserAddress/MYSELF',)
-# print_response_information(response_post)
